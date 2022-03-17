3c642a82d221564cff0fd4b56cb9850f119dfc71
silvio-jr <silvio.lima.p.jr@gmail.com>
2022-03-17 15:07:18 -0300

    comentários + correções

--- js/carousel.js
+++ js/carousel.js
@@ -18,11 +18,10 @@ class Carousel {
         if(arr){
 
             if(arr.length > 0){
-                Carousel._sequence = 0;
+                Carousel._sequence = 0; //contador
                 Carousel._size = arr.length;
                 Carousel.insertContent(arr);
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next();},2000);
+                Carousel._interval = setInterval(function(){ Carousel.Next();},2000); //delay 2s
             }
             
         } else {
@@ -38,7 +37,7 @@ class Carousel {
         const carouselDiv = document.querySelector("#carousel");
         const carouselTitleDiv = document.querySelector("#carousel-title");
         
-        arr.forEach( function(arrElement){
+        arr.forEach( function(arrItem){ //para cada instância Carousel no array, criar uma div.slide e uma div.title
 
             const slideDiv = document.createElement("div");
             slideDiv.classList.add("slide");
@@ -49,18 +48,18 @@ class Carousel {
             const img = document.createElement("img");
             const a = document.createElement("a");
 
-            img.src = arrElement._image;
-            a.textContent = arrElement._title;
-            a.href = arrElement._url;
+            img.src = arrItem._image; 
+            a.textContent = arrItem._title; 
+            a.href = arrItem._url;
             
             if (carouselDiv.childNodes.length === 1) slideDiv.dataset.active = true; //define o primeiro slide como active
-            if (carouselTitleDiv.childNodes.length === 1) titleDiv.dataset.titleDisplay = true; //define o primeiro título como active
+            if (carouselTitleDiv.childNodes.length === 1) titleDiv.dataset.titleDisplay = true; //define o primeiro título como display
 
             slideDiv.append(img)
-            carouselDiv.append(slideDiv);
+            carouselDiv.append(slideDiv); //o documento html recebe a div criada com o conteúdo das imagens
             
             titleDiv.append(a)
-            carouselTitleDiv.append(titleDiv);
+            carouselTitleDiv.append(titleDiv); //o documento html recebe a div criada com o conteúdo dos títulos
         })
     }
 
@@ -69,20 +68,20 @@ class Carousel {
         * Metodo responsável por exibir o conteúdo na tela e incrementar o contador. 
         */
         
-        const slides = document.querySelectorAll(".slide");
-        const activeSlide = document.querySelector("[data-active]");
+        const slides = document.querySelectorAll(".slide"); //seleciona todas as divs com a classe slide
+        const activeSlide = document.querySelector("[data-active]"); //seleciona o slide com o data attribute active (este é o slide que está sendo exibido)
 
-        const titles = document.querySelectorAll(".title");
-        const activeTitle = document.querySelector("[data-title-display]")
+        const titles = document.querySelectorAll(".title"); //seleciona todas as divs com a classe title
+        const displayedTitle = document.querySelector("[data-title-display]") //seleciona o título com o data attribute title-display (este é o título que está sendo exibido)
         
-        Carousel._sequence = [...slides].indexOf(activeSlide) + 1; //incremento no contador
+        Carousel._sequence = [...slides].indexOf(activeSlide) + 1; //retorna o index do slide ativo e incrementa o contador
 
-        if (Carousel._sequence >= Carousel._size) Carousel._sequence = 0;
+        if (Carousel._sequence >= Carousel._size) Carousel._sequence = 0; //zera ocontador caso seu valor extrapole o tamanho do array
         
-        [...slides][Carousel._sequence].dataset.active = true;
-        delete activeSlide.dataset.active;
+        [...slides][Carousel._sequence].dataset.active = true; //define o próximo slide como ativo
+        delete activeSlide.dataset.active; //desativa o slide anterior
 
-        [...titles][Carousel._sequence].dataset.titleDisplay = true;
-        delete activeTitle.dataset.titleDisplay;
+        [...titles][Carousel._sequence].dataset.titleDisplay = true; //permite que o título referente ao slide mostrado no momento seja exibido
+        delete displayedTitle.dataset.titleDisplay; //desativa o título anterior
     }
 };
