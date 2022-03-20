4c454539d82aa8292c6b2e696d3f19e0c04fa205
silvio-jr <silvio.lima.p.jr@gmail.com>
2022-03-20 03:50:29 -0300

    setCarToCompare implementada

--- js/compare.js
+++ js/compare.js
@@ -6,7 +6,17 @@ class Car {
    
 
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+       this.nome = nome;
+       this.preco = preco;
+       this.alturaCacamba = alturaCacamba;
+       this.alturaVeiculo = alturaVeiculo;
+       this.alturaSolo = alturaSolo;
+       this.capacidadeCarga = capacidadeCarga;
+       this.motor = motor;
+       this.potencia = potencia;
+       this.volumeCacamba = volumeCacamba;
+       this.roda = roda;
+       this.image = image;
     }
 } 
 
@@ -23,10 +33,15 @@ function SetCarToCompare(el, carClass) {
    
     if(carClass instanceof Car){       
         if(el.checked){
-                
+            if(carArr.length > 1) {
+                el.checked = false;
+            }
+            else{
+                carArr.push(carClass);
+            }
             
         } else {
-          
+            carArr.splice(GetCarArrPosition(carArr, carClass),1);
         } 
     } else {
         throw "You need set a Car Class";
@@ -44,9 +59,20 @@ function ShowCompare() {
 }
 
 function HideCompare(){
+    //quando ocurtar a página, deletar o conteúdo da tabela no documento html
     document.getElementById("compare").style.display = "none"; 
 }
 
 function UpdateCompareTable() {
-    
+    //corrigir formatação das imagens
+    img0 = document.createElement("img");
+    img0.src = carArr[0].image;
+    img0.alt = "veiculo 1";
+    img1 = document.createElement("img");
+    img1.src = carArr[1].image;
+    img1.alt = "veiculo 2";
+    document.getElementById("compare_image_0").appendChild(img0);
+    document.getElementById("compare_image_1").appendChild(img1);
+
 }
+
