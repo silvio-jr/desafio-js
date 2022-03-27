9b995da91944b5f195dc7d23daa82daf57028441
silvio-jr <silvio.lima.p.jr@gmail.com>
2022-03-27 15:35:41 -0300

    desafio finalizado

--- js/form.js
+++ js/form.js
@@ -20,19 +20,24 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
-  
 }
 
 function Enviar() {
 
-    var nome = document.getElementById("nomeid");
+    const formData = [...document.querySelectorAll(".fordform")];
+    const errorElement = document.querySelector("#error");
+    let messages = [];
+    
+    formData.forEach(arrItem => {
+        if (arrItem.value === '' || arrItem.value === null) messages.push(arrItem.name); 
+    });
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    if(formData[5].value === "COMO DESEJA SER CONTATADO") messages.push(formData[5].name);
+
+    if (messages.length === 0){
+        alert('Obrigado sr(a) ' + formData[0].value + ' os seus dados foram encaminhados com sucesso');
+        errorElement.innerText = "";
+    } else{
+        errorElement.innerText = "*Campo(s) obrigatório(s): " + messages.join(", ") + ".";
     }
 }
-
-//validar email
-//colocar telefone no formato certo
-//fazer animação de campo obrigatório caso os elementos não estejam preenchidos
-//decobrir o porquê da mensagem de enviado não estar aparecendo
