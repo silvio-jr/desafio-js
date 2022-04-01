be51895cccf9da6caccf351faa55b3ffb31ee347
silvio-jr <silvio.lima.p.jr@gmail.com>
2022-04-01 00:03:56 -0300

    corrigindo pré-requisitos do projeto

--- js/form.js
+++ js/form.js
@@ -2,24 +2,22 @@
 //class contato
 
 class contato {
-    constructor(nome,sobrenome,email,cpf,telefone,contato){
+    constructor(nome,email,telefone,contato,mensagem){
         this.nome = nome;
-        this.sobrenome = sobrenome;
         this.email = email;
-        this.cpf = cpf;
         this.telefone = telefone;
         this.contato = contato;
+        this.mensagem = mensagem;
     }
 }
 
 function Post(form) {
 
   let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
             form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
+            form.elements.namedItem("contato").value,
+            form.elements.namedItem("mensagem").value);
 }
 
 function Enviar() {
